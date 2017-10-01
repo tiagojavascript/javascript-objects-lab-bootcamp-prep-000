e1f8a7350a80e62ca259de531571e87ea10a587c
Tiago Lopes <tiagojavascript@gmail.com>
2017-10-01 22:28:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 };
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key]: value });
+}
